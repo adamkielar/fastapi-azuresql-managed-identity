6499be3c98ed3d79430b2d5a4675e31b4e90bac8
Adam Kielar <adam.kielar@netguru.com>
2022-02-06 17:25:17 +0100

    Initial setup (#2)
    * Initial setup
    
    * WIP
    
    * WIP2
    
    * WIP3
    
    * WIP4

--- app/api/api_v1/endpoints/health.py
+++ app/api/api_v1/endpoints/health.py
@@ -6,17 +6,29 @@ from fastapi import Depends
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 
-from database_interface.mssql.session import DBSessionManager
+from database_interface.mssql.session import MssqlSessionManager
+from database_interface.postgres.session import PostgresSessionManager
 
 router = APIRouter()
 
 
-async def get_db_session() -> Generator:
-    with DBSessionManager() as db_session:
-        yield db_session
+async def get_mssql_session() -> Generator:
+    with MssqlSessionManager() as mssql_db_session:
+        yield mssql_db_session
+
+
+async def get_postgres_session() -> Generator:
+    with PostgresSessionManager() as postgres_db_session:
+        yield postgres_db_session
 
 
 @router.get("/mssql_db")
-async def mssql_db_health(db_session: Session = Depends(get_db_session, use_cache=False)) -> Dict[str, str]:
+async def mssql_db_health(db_session: Session = Depends(get_mssql_session)) -> Dict[str, str]:
     db_version = db_session.execute(text("SELECT @@version")).scalar()
     return {"mssql version": db_version}
+
+
+@router.get("/postgres_db")
+async def postgres_db_health(db_session: Session = Depends(get_postgres_session)) -> Dict[str, str]:
+    db_version = db_session.execute(text("SELECT version()")).scalar()
+    return {"postgres version": db_version}
--- database_interface/postgres/session.py
+++ database_interface/postgres/session.py
@@ -3,23 +3,37 @@ from types import TracebackType
 from typing import Optional
 from typing import Type
 
-import pyodbc
+from azure.core.credentials import AccessToken
+from azure.identity import DefaultAzureCredential
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker
 
 from database_interface.config import db_settings
 
-pyodbc.pooling = False
 
-DATABASE_URL = URL.create("mssql+pyodbc", query={"odbc_connect": db_settings.MSSQL_CONNECTION_STRING})
+def get_aad_token():
+    scope: str = "https://ossrdbms-aad.database.windows.net/.default"
+    credential: DefaultAzureCredential = DefaultAzureCredential()
+    access_token: AccessToken = credential.get_token(scope)
+    return access_token.token
+
+
+DATABASE_URL = URL.create(
+    "postgresql+psycopg2",
+    username=db_settings.POSTGRES_USERNAME,
+    password=get_aad_token(),
+    host=db_settings.POSTGRES_HOSTNAME,
+    port=db_settings.POSTGRES_PORT,
+    database=db_settings.POSTGRES_DB
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, future=True)
 
 
 @dataclass
-class MssqlSessionManager:
+class PostgresSessionManager:
     def __post_init__(self) -> None:
         self.db_session = SessionLocal()
 
